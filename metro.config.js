7f910224b88b919f91c56f7639f32576f030e55f
sirius-ashwak <mohamedashwak2022@gmail.com>
2025-08-21 18:01:32 +0530

    Render metro package

--- metro.config.js
+++ metro.config.js
@@ -2,18 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Completely disable New Architecture and TurboModules
+// Keep it simple for web builds
 config.resolver.unstable_enableSymlinks = false;
-config.resolver.unstable_enablePackageExports = false;
-
-// Force legacy architecture
-config.transformer = {
-  ...config.transformer,
-  unstable_allowRequireContext: false,
-  hermesParser: false,
-};
-
-// Disable experimental features that might trigger TurboModules
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
 module.exports = config;
